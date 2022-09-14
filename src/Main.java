4cb3f29c3576d66e2359a4bcaeec406d92ed509d
Victor Petrunin <petruninviktor9464@gmail.com>
2022-09-14 14:08:21 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 14_382;
+
+        int bonus = price / 20;
+        System.out.println("Бонус:" + bonus);
+    }
+}
